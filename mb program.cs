d0e19d62c132a8253a59037b54619530ca09c8f7
mohammed7091 <145067341+mohammed7091@users.noreply.github.com>
2024-06-28 22:55:16 +0100

    Add files via upload

---
+++ mb program.cs
@@ -0,0 +1,16 @@
+﻿// Exercise file for LinkedIn Learning Course .NET Programming with C# Tijjani 
+Abdullahi
+// Example file for .NET Data Types
+// Declare some types with values
+int a = 1;
+char c = 'A';
+float f = 123.45f;
+decimal d = 400.85m;
+int b = default;
+bool tf = default;
+Console.WriteLinxe($"{a}, {b}, {tf}, {c}, {f}, {d}");
+// TODO: implicit type conversion
+Console.WriteLine($"{c + a}");
+Console.WriteLine($"{(char)(c + a)}");
+Console.WriteLine($"{f + a}");
+Console.WriteLine($"{f + c}");
